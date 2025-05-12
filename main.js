fa68615c392a655b852735e2a0db6c3d4bf021d6
Orhan Cansu <65315135+konerjonlar@users.noreply.github.com>
2025-05-12 16:37:59 +0300

    add | menu & css & js

---
+++ main.js
@@ -0,0 +1,32 @@
+const trigger = document.querySelector("menu > .trigger");
+trigger.addEventListener('click', (e) => {
+  e.currentTarget.parentElement.classList.toggle("open");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.hero-main-slider', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    }
+  });
+
+  // Scroll Top
+  const scrollTopBtn = document.getElementById('scrollTop');
+  scrollTopBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
